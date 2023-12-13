98207b6db2e4e2afca148ed23978c328d2a9b42e
nick <nikole.phipps12@gmail.com>
2023-12-13 12:49:13 -0500

    app base made

--- FinalExam-Practical/eventManagerApp.py
+++ FinalExam-Practical/eventManagerApp.py
@@ -2,4 +2,26 @@ from event import Events
 
 
 def run():
-    pass
+    global event
+    event = Events.loadDataFromFile
+ 
+    while True:
+        menu = print('1.Add regular Event \n2.Add Special Event \n3.Calculate All Event Fees 4.Exit')
+        choice = int(input('What would you like to do(1-4)?: '))
+
+        if choice == 1:
+            category = 'Regular Event'
+
+        if choice == 2:
+            pass
+        
+        if choice == 3:
+            pass
+
+        if choice ==4:
+            print('Thank you!')
+            exit()
+
+
+
+
