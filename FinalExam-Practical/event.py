ea0204716a81821fe2552a7f57c14609ad9cce0e
nick <nikole.phipps12@gmail.com>
2023-12-13 12:13:58 -0500

    base done

---
+++ FinalExam-Practical/event.py
@@ -0,0 +1,18 @@
+import json 
+from collections import defaultdict
+
+class Events:
+
+    events_file = 'events.json'
+
+    event = defaultdict(list)
+
+    def add_event(eventID, eventLocation):
+        pass
+
+    def calculateEventFee():
+        pass
+
+    
+
+
