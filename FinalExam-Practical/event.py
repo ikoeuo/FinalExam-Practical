aea5d9a5454e2363b517af2c8a9fcc08d29665e1
nick <nikole.phipps12@gmail.com>
2023-12-13 12:33:35 -0500

    calculate evnt fee added

--- FinalExam-Practical/event.py
+++ FinalExam-Practical/event.py
@@ -7,12 +7,33 @@ class Events:
 
     event = defaultdict(list)
 
-    def add_event(eventID, eventLocation):
-        pass
+    def loadDataFromFile(events_file, event):
+        try:
+            with open(events_file, 'r') as file:
+                return json.load(file)
+            
+        except FileNotFoundError:
+            return defaultdict(list)
+
+
+    def saveDataToFile(events_file, event):
+        with open(events_file, 'w') as file:
+            json.dump(event, file)
+
+    def add_event(eventType , eventID = str, eventLocation = str, category = str):
+        eventList = {'Event ID' : eventID, 'Event Location' : eventLocation, 'Category' : category}
+        super.event[eventType].append(eventList)
+        super.saveDatatoFile()
+        print(f'{eventType} has been added to Events!')
+        
 
     def calculateEventFee():
-        pass
-
+        Fee = 0
+        eventFee = Fee * 1.13
+        return eventFee
     
+
+
+
 
 
